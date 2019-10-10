895bb804548d8143c0db4ed9dac84b0cedb288b0
rafaelserra <53586334+rafaelserra@users.noreply.github.com>
2019-10-09 21:49:07 -0300

    Add files via upload

---
+++ cod1.js
@@ -0,0 +1,64 @@
+/* Exercicio 1 */
+
+function funcao1(){
+    alert("Alerta de JavaScript");
+}
+
+
+
+
+
+/* Exercicio 2 */
+    
+var nomeAluno1 = "Joao da Silva";
+
+var result1 = 0.6;
+
+var nota1 = 7;
+
+var result2 = 0.4;
+
+var nota2 = 8.5;
+
+var result1 = nota1 * result1;
+
+var result2 = nota2 * result2;
+
+var media = result1 + result2;
+
+alert("Ola " +  nomeAluno1 + " sua nota 1 e : " + nota1  + ", sua nota 2 e : ," + nota2);
+
+alert("Ola " +  nomeAluno1 + " sua nota total e : " + media);
+
+
+
+
+
+/* Exercicio 3 */
+
+function funcao2(){
+
+var nomes = ['Joao da Silva', 'Jose de Andrade', 'Maria do Nascimento', 'Zeze Legal'];
+
+var notas1 = ['7', '5', '9', '3'];
+
+var notas2 = ['8.5', '7.8', '5.5', '4'];
+
+var final = ['7.6', '6.1', '7.6', '3.4'];
+
+}
+
+
+
+
+
+
+/* Exercicio 4 */
+
+var resultJs = "aprovado";
+
+var resultJa = "reforco";
+
+var resultM = "aprovado";
+
+var resultZ = "reprovado";
